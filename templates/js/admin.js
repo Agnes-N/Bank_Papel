daf9401ab67dd8e131b9bd0a200c1086ffc908e1
Agnes <aggyreina@gmail.com>
2020-02-13 08:10:31 +0200

    feat(delete user account): add delete feature  - add delete user account UI  - add admin.js file  - modified admin UI  [Finished #171231033]

--- templates/js/admin.js
+++ templates/js/admin.js
@@ -1,25 +1,59 @@
-// Get the modal
 var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
+
+function removeDummy() {
+  var elem = document.getElementById('dummy');
+  elem.parentNode.removeChild(elem);
+  return false;
+}
+
+// Helper, go from el up to ancestor with tagName
+function upTo(el, tagName) {
+  tagName = tagName.toLowerCase();
+  while (el = el.parentNode) {
+    if (el.tagName && el.tagName.toLowerCase() == tagName) {
+      return el;
+    }
+  }
+  return null;
+}
+
+// Delete rows with checked checkboxes
+function deleteRows() {
+  var cbs = document.querySelectorAll('#table input:checked'); 
+  [].forEach.call(cbs, function(cb) {
+      var row = upTo(cb, 'tr');
+      row && row.parentNode.removeChild(row);
+  });
+}
+
+// var Mainarray = [];
+// var i = 0;
+// $("#delete_row").click(function(){
+//   if(i>=0){
+//    $("#addr"+(i-1)).html('');
+//    if(i > 0)
+//    {
+//      i--;
+//    }
+//  }
+// });
+// function delrow(sender)
+// {
+//  $(sender).parent().parent().remove();
+// }
--- templates/js/user_login.js
+++ templates/js/user_login.js
@@ -1,131 +1,21 @@
-var emailArray=[];
-var passwordArray=[];
 
-var loginBox = document.getElementById("login");
-var regBox = document.getElementById("register");
-var forgetBox = document.getElementById("forgot");
-
-var loginTab = document.getElementById("lt");
-var regTab = document.getElementById("rt");
-
-function regTabFun(){
-    event.preventDefault();
-
-    regBox.style.visibility="visible";
-    loginBox.style.visibility="hidden";
-    forgetBox.style.visibility="hidden";
-
-    // regTab.style.backgroundColor="";
-    // loginTab.style.backgroundColor="rgba(11, 177, 224, 0.82)";
-}
-function loginTabFun(){
-    event.preventDefault();
-
-    regBox.style.visibility="hidden";
-    loginBox.style.visibility="visible";
-    forgetBox.style.visibility="hidden";
-
-    // loginTab.style.backgroundColor="rgb(12, 132, 189)";
-    // regTab.style.backgroundColor="rgba(11, 177, 224, 0.82)";
-}
-function forTabFun(){
-    event.preventDefault();
-
-    regBox.style.visibility="hidden";
-    loginBox.style.visibility="hidden";
-    forgetBox.style.visibility="visible";
-
-    // regTab.style.backgroundColor="rgba(11, 177, 224, 0.82)";
-    // loginTab.style.backgroundColor="rgba(11, 177, 224, 0.82)";
-
-}
-
-
-function register(){
-    event.preventDefault();
-
-    var email = document.getElementById("re").value;
-    var password = document.getElementById("rp").value;
-    var passwordRetype = document.getElementById("rrp").value;
-
-    if (email == ""){
-        alert("Email required.");
-        return ;
-    }
-    else if (password == ""){
-        alert("Password required.");
-        return ;
-    }
-    else if (passwordRetype == ""){
-        alert("Password required.");
-        return ;
-    }
-    else if ( password != passwordRetype ){
-        alert("Password don't match retype your Password.");
-        return;
-    }
-    else if(emailArray.indexOf(email) == -1){
-        emailArray.push(email);
-        passwordArray.push(password);
-
-        alert(email + "  Thanks for registration. \n Go to log in then!!");
-
-        document.getElementById("re").value ="";
-        document.getElementById("rp").value="";
-        document.getElementById("rrp").value="";
-    }
-    else{
-        alert(email + " is already register.");
-        return ;
-    }
-}
-function login(){
-    event.preventDefault();
-
-    var email = document.getElementById("se").value;
-    var password = document.getElementById("sp").value;
-
-    var i = emailArray.indexOf(email);
-
-    if(emailArray.indexOf(email) == -1){
-        if (email == ""){
-            alert("Email required.");
-            return ;
+const loginFuction = () => {
+    const submit = document.querySelector(".submit");
+    const logIn = document.querySelector("#login")
+    const email = document.querySelector("#email");
+    const password = document.querySelector("#password");
+    const adminEmail = "a@gmail.com";
+    const adminPassword = "12";
+    const cashierEmail = "c@gmail.com";
+    const cashierPassword = "123"
+  
+    submit.addEventListener("click", () => {
+        if(email.value === adminEmail && password.value === adminPassword){
+            logIn.setAttribute("action", "admin.html");
         }
-        alert("Email does not exist.");
-        return ;
-    }
-    else if(passwordArray[i] != password){
-        if (password == ""){
-            alert("Password required.");
-            return ;
+        if(email.value === cashierEmail && password.value === cashierPassword){
+            logIn.setAttribute("action", "cashier.html");
         }
-        alert("Password does not match.");
-        return ;
-    }
-    else {
-        // alert(email + " yor are login Now \n welcome to Papel.");
-
-        document.getElementById("se").value ="";
-        document.getElementById("sp").value="";
-        return ;
-    }
-
-}
-function forgot(){
-    event.preventDefault();
-
-    var email = document.getElementById("fe").value;
-
-    if(emailArray.indexOf(email) == -1){
-        if (email == ""){
-            alert("Email required.");
-            return ;
-        }
-        alert("Email does not exist.");
-        return ;
-    }
-
-    alert("email is send to your email check it out. \n Thanks");
-    document.getElementById("fe").value ="";
-}  
+    })
+  }
+  loginFuction();
